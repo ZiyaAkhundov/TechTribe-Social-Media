7a9177546f8256f369172bdb49da91cb2028b159
ZiyaAkhundov <akhundovziya04@gmail.com>
2023-09-14 18:46:58 +0400

    fixed csrf token

--- Backend/middleware/csrfProtection.js
+++ Backend/middleware/csrfProtection.js
@@ -1,6 +1,6 @@
-const csrfProtection = (req, res, next) => {
-    const csrfTokenFromCookie = req.cookies.csrf-token;
-    console.log('csrf: '+ req.cookies.csrf-token)
+ const csrfProtection = (req, res, next) => {
+    const csrfTokenFromCookie = req.cookies['csrf-token'];
+    console.log('csrf: '+ csrfTokenFromCookie)
     if (!csrfTokenFromCookie) {
       return res.status(403).json({ message: 'Invalid CSRF token', success: false });
     }
