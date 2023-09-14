3d20e09e1a004535020017a8e03840cb8b6c0d04
ZiyaAkhundov <akhundovziya04@gmail.com>
2023-09-14 18:39:12 +0400

    update csrf-protection

--- Backend/middleware/csrfProtection.js
+++ Backend/middleware/csrfProtection.js
@@ -1,10 +1,8 @@
 const csrfProtection = (req, res, next) => {
-    const csrfTokenFromCookie = req.cookies['csrf-token'];
-    const csrfTokenFromClient = req.headers['x-csrf-token'];
-    if (!csrfTokenFromCookie || csrfTokenFromCookie !== csrfTokenFromClient) {
+    const csrfTokenFromCookie = req.cookies.csrf-token;
+    if (!csrfTokenFromCookie) {
       return res.status(403).json({ message: 'Invalid CSRF token', success: false });
     }
-  
     next();
   }
   
