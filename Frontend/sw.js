615ba5807b95b5540b7d7e41323505cdcc840f0e
ZiyaAkhundov <akhundovziya04@gmail.com>
2023-09-17 15:53:22 +0400

    sw.js

--- Frontend/sw.js
+++ Frontend/sw.js
@@ -1,2 +1,30 @@
-self.addEventListener('install', e => console.log('pwa installed.'));
-self.addEventListener('fetch', event => {});
+const CACHE_NAME = 'my-cache-v1';
+const urlsToCache = [
+  '/',
+  './index.html', 
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then((response) => {
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, response.clone());
+        });
+
+        return response;
+      });
+    })
+  );
+});
