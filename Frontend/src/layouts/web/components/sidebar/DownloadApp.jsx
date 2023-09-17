e4e377f3d66e1ee0ba01cefa7c1d608a658f590a
ZiyaAkhundov <akhundovziya04@gmail.com>
2023-09-17 16:49:46 +0400

    fix

--- Frontend/src/layouts/web/components/sidebar/DownloadApp.jsx
+++ Frontend/src/layouts/web/components/sidebar/DownloadApp.jsx
@@ -1,25 +1,42 @@
-import React from 'react'
-import download from '../../../../assets/image/download.svg'
+import React, { useEffect, useState } from 'react';
+import download from '../../../../assets/image/download.svg';
+
 export default function DownloadApp() {
-  const installButton = document.getElementById('install-app');
-  if(installButton){
-    let beforeInstallPromptEvent
-    window.addEventListener("beforeinstallprompt", function(e) {
-        e.preventDefault();
-        beforeInstallPromptEvent = e
-        installButton.style.display = 'flex'
-        installButton.addEventListener("click", function() {
-            e.prompt();
-        });
-        installButton.hidden = false;
-    });
-    installButton.addEventListener("click", function() {
+  const [showInstallButton, setShowInstallButton] = useState(false);
+  let beforeInstallPromptEvent;
+
+  useEffect(() => {
+    const installButton = document.getElementById('installApp');
+
+    const beforeInstallHandler = (e) => {
+      e.preventDefault();
+      beforeInstallPromptEvent = e;
+      setShowInstallButton(true);
+    };
+
+    const installButtonClickHandler = () => {
+      if (beforeInstallPromptEvent) {
         beforeInstallPromptEvent.prompt();
-    });
-  }
+      }
+    };
+
+    window.addEventListener('beforeinstallprompt', beforeInstallHandler);
+    installButton.addEventListener('click', installButtonClickHandler);
+
+    return () => {
+      window.removeEventListener('beforeinstallprompt', beforeInstallHandler);
+      installButton.removeEventListener('click', installButtonClickHandler);
+    };
+  }, []);
+
   return (
-    <div className='h-10 flex justify-center items-center flex-shrink-0 bg-slate-900 text-white rounded-sm mb-1 mx-1 cursor-pointer' id='install-app'>
-       <img src={download} className='h-4 mx-1' alt="" /> Download App
+    <div
+      className={`h-10 flex justify-center items-center flex-shrink-0 bg-slate-900 text-white rounded-sm mb-1 mx-1 cursor-pointer ${
+        showInstallButton ? '' : 'hidden'
+      }`}
+      id="installApp"
+    >
+      <img src={download} className="h-4 mx-1" alt="" /> Download App
     </div>
-  )
+  );
 }
