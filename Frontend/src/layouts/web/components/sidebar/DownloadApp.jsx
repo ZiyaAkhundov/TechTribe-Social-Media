a4d782d63983a82bdaeeaafbe7c5fdedd937f1ab
ZiyaAkhundov <akhundovziya04@gmail.com>
2023-09-17 17:06:11 +0400

    test

--- Frontend/src/layouts/web/components/sidebar/DownloadApp.jsx
+++ Frontend/src/layouts/web/components/sidebar/DownloadApp.jsx
@@ -21,28 +21,25 @@ export default function DownloadApp() {
     };
 
     window.addEventListener('beforeinstallprompt', beforeInstallHandler);
-    if(installButton){
+
+    // Check if the installButton element exists before adding an event listener
+    if (installButton) {
       installButton.addEventListener('click', installButtonClickHandler);
     }
 
     return () => {
       window.removeEventListener('beforeinstallprompt', beforeInstallHandler);
-      installButton.removeEventListener('click', installButtonClickHandler);
+
+      // Check if the installButton element exists before removing the event listener
+      if (installButton) {
+        installButton.removeEventListener('click', installButtonClickHandler);
+      }
     };
   }, []);
 
   return (
-    <div>
-      {showInstallButton ? (
-        <div
-          className="h-10 flex justify-center items-center flex-shrink-0 bg-slate-900 text-white rounded-sm mb-1 mx-1 cursor-pointer"
-          id="installApp"
-        >
-          <img src={download} className="h-4 mx-1" alt="" /> Download App
-        </div>
-      ) : (
-        <p>Your PWA is installed!</p>
-      )}
+    <div className="h-10 flex justify-center items-center flex-shrink-0 bg-slate-900 text-white rounded-sm mb-1 mx-1 cursor-pointer" id="installApp">
+      <img src={download} className="h-4 mx-1" alt="" /> Download App
     </div>
   );
 }
