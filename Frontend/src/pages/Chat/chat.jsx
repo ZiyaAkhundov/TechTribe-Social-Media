6dc19db866ca279cc6bd16a184e5447c3882ea2b
ZiyaAkhundov <akhundovziya04@gmail.com>
2023-09-21 20:54:01 +0400

    fix

--- Frontend/src/pages/Chat/chat.jsx
+++ Frontend/src/pages/Chat/chat.jsx
@@ -63,6 +63,7 @@ export default function chat({func}) {
 
     useEffect(()=>{
       setMessages('')
+      setDisable(true)
       const getRoom = async()=>{
          const response = await getARoom(roomId)
          if(response.status=="success"){
