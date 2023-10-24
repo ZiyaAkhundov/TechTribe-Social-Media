d20c99defd8468c562aaf7ff4e631b55680ea185
ZiyaAkhundov <akhundovziya04@gmail.com>
2023-10-24 17:31:56 +0400

    cookie server.js

--- Backend/server.js
+++ Backend/server.js
@@ -91,7 +91,7 @@ app.use(
       httpOnly: true,
       secure: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: 'none',
+      sameSite: 'Strict',
       domain:'techtribe-api.onrender.com'
     },
     name: 'techtribe_session',
